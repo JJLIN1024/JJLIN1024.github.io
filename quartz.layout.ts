69eb4e12944dbbd2f1b487a320c49f835457006a
林家頡 <jimmy@linjiajiadeMacBook-Air.local>
2024-02-26 12:25:51 +0800

    Quartz sync: Feb 26, 2024, 12:25 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
-    Component.RecentNotes({ title: "Recent Notes", limit: 3})
   ],
   right: [
     Component.Graph(),
